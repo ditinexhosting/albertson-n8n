ad8ba4d3ac6282904262f4b0ec5538a56231dedd
mohsinking2002 <mohsinraja4667@gmail.com>
2026-01-01 01:44:24 +0530

    created services templates for reusable apis

---
+++ packages/frontend/albertsons/src/services/projects.service.ts
@@ -0,0 +1,28 @@
+import { albertsonsRestApiRequest } from '@src/utils/albertsonsRestApiRequest';
+
+/**
+ * Fetch project details by project id
+ *
+ * @param projectId - Unique project id
+ * @returns Project details list
+ */
+export const getProjectDetails = (projectId: string) =>
+	albertsonsRestApiRequest('GET', `/v1/projects/${projectId}`);
+
+/**
+ * Fetch project list for logged in user
+ *
+ * @param userId - Unique user id
+ * @returns Project list
+ */
+export const getAllProjects = (userId: string) =>
+	albertsonsRestApiRequest('GET', `/v1/projects/all`, { userId });
+
+/**
+ * Map agents to project
+ *
+ * @param payload - Array of agentId(s)
+ * @returns Mapped data Count(s)
+ */
+export const addAgentsToProject = (projectId: string, payload: any) =>
+	albertsonsRestApiRequest('POST', `/v1/projects/${projectId}/add-agents`, payload);
