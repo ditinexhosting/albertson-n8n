8946849b1f806c292eb4793adf6df28faa4d2203
ditinex <asifakramsk@gmail.com>
2026-01-16 02:18:10 +0530

    custom code removed from fork

--- packages/frontend/albertsons/src/utils/helper.ts
+++
@@ -1,6 +0,0 @@
-export function getProgressStatus(percentage) {
-	if (percentage === 0) return 'default';
-	if (percentage <= 30) return 'error';
-	if (percentage <= 80) return 'warning';
-	return 'success';
-}
